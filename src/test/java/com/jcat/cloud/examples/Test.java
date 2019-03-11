230ff38b6b5f4545586ee8f0399a155418264673
root <root@instance-1.asia-south1-c.c.even-metrics-234003.internal>
2019-03-11 11:13:03 +0000

    java test 1

---
+++ src/test/java/com/jcat/cloud/examples/Test.java
@@ -0,0 +1,10 @@
+package com.jcat.cloud.examples;
+
+public class Test  {
+
+@Test(enabled = true)
+    public void openStackReconnectsAfterCicModeChange() {
+
+        System.out.println("Hello world from drone/jcat");
+    }
+
